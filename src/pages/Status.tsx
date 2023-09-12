a9d97dc926719c30f20a89472c7004c92a8c59cd
Bruno Lima <brunolimadevelopment@gmail.com>
2023-09-12 00:04:49 -0300

    answer tweet

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,15 +1,28 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 import './Status.css'
 
-const answers = [
-    'Concordo...',
-    'Olha, faz sentido!',
-    'Parabéns pelo progresso'
-]
 
 export function Status() {
+
+
+    const [newAnswer, setNewAnswer] = useState('')
+
+    const [answers, setAnswers] = useState([
+        'Concordo...',
+        'Olha, faz sentido!',
+        'Parabéns pelo progresso'
+    ])
+
+    function createNewAnswer(event: FormEvent) {
+        event.preventDefault()
+
+        setAnswers([newAnswer, ...answers])
+        setNewAnswer('')
+    }
+
     return (
         <main className="status">
             <Header title="Tweet" />
@@ -18,10 +31,12 @@ export function Status() {
 
             <Separator />
 
-            <form className="answer-tweet-form">
+            <form onSubmit={createNewAnswer} className="answer-tweet-form">
                 <label htmlFor="tweet">
                     <img src="https://avatars.githubusercontent.com/u/20023147?v=4" alt="Bruno Lima" />
-                    <textarea id="tweet" placeholder="Tweet your answer" />
+                    <textarea onChange={(event) => {
+                        setNewAnswer(event.target.value)
+                    }} value={newAnswer} id="tweet" placeholder="Tweet your answer" />
                 </label>
 
                 <button type="submit">Answer</button>
